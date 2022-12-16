ac1b6976e14c280cc214e0ac2d837b6bb2cd2630
Sebastian Struß <struss@justtrack.io>
2022-12-16 12:29:05 +0100

    feat: Added functionality

---
+++ examples/basic/main.tf
@@ -0,0 +1,12 @@
+module "example" {
+  source = "../.."
+
+  name                        = "hello-world"
+  app_image_repository        = "hello-world"
+  app_image_tag               = "latest"
+  log_router_image_repository = "fluent/fluent-bit"
+  log_router_image_tag        = "1.9"
+  ecs_cluster_arn             = "arn:aws:ecs:eu-central-1:123456789123:cluster/my-cluster"
+  schedule_expression         = "cron(* * * * ? *)"
+  region                      = "eu-central-1"
+}
---
+++ variables.tf
@@ -0,0 +1,239 @@
+variable "log_router_container_cpu" {
+  type        = string
+  description = "The log router cpu reservation for the ECS task definition"
+  default     = 30
+}
+
+variable "log_router_container_memory_reservation" {
+  type        = string
+  description = "The log router cpu reservation for the ECS task definition"
+  default     = 64
+}
+
+variable "port_metadata" {
+  type        = number
+  description = "Define the metadata port"
+  default     = 8070
+}
+
+variable "port_gateway" {
+  type        = number
+  description = "Define the gateway port"
+  default     = 8088
+}
+
+variable "port_health" {
+  type        = number
+  description = "Define the health port"
+  default     = 8090
+}
+
+variable "port_profiling" {
+  type        = number
+  description = "Define the profiling port"
+  default     = 8091
+}
+
+variable "working_directory" {
+  type        = string
+  description = "The working directory to run commands inside the container"
+  default     = "/app"
+}
+
+variable "app_image_repository" {
+  type        = string
+  description = "Container registry repository url"
+  default     = ""
+}
+
+variable "app_image_tag" {
+  type        = string
+  description = "The default container image to use in container definition"
+  default     = null
+}
+
+variable "log_router_image_repository" {
+  type        = string
+  description = "Container registry repository url"
+  default     = ""
+}
+
+variable "log_router_image_tag" {
+  type        = string
+  description = "The default container image to use in container definition"
+  default     = null
+}
+
+variable "container_cpu" {
+  type        = number
+  description = "The vCPU setting to control cpu limits of container. (If FARGATE launch type is used below, this must be a supported vCPU size from the table here: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html)"
+  default     = 0
+}
+
+variable "container_memory" {
+  type        = number
+  description = "The amount of RAM to allow container to use in MB. (If FARGATE launch type is used below, this must be a supported Memory size from the table here: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html)"
+  default     = null
+}
+
+variable "container_start_timeout" {
+  type        = number
+  description = "Time duration (in seconds) to wait before giving up on resolving dependencies for a container"
+  default     = null
+}
+
+variable "container_stop_timeout" {
+  type        = number
+  description = "Time duration (in seconds) to wait before the container is forcefully killed if it doesn't exit normally on its own"
+  default     = null
+}
+
+variable "task_cpu" {
+  type        = number
+  description = "The number of CPU units used by the task. If unspecified, it will default to `container_cpu`. If using `FARGATE` launch type `task_cpu` must match supported memory values (https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size)"
+  default     = null
+}
+
+variable "task_memory" {
+  type        = number
+  description = "The amount of memory (in MiB) used by the task. If unspecified, it will default to `container_memory`. If using Fargate launch type `task_memory` must match supported cpu value (https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size)"
+  default     = null
+}
+
+variable "task_policy_arns" {
+  type        = list(string)
+  description = "A list of IAM Policy ARNs to attach to the generated task role."
+  default     = []
+}
+
+variable "ulimits" {
+  type = list(object({
+    name      = string
+    softLimit = number
+    hardLimit = number
+  }))
+  description = "The ulimits to configure for the container. This is a list of maps. Each map should contain \"name\", \"softLimit\" and \"hardLimit\""
+  default     = []
+}
+
+variable "container_memory_reservation" {
+  type        = number
+  description = "The amount of RAM (Soft Limit) to allow container to use in MB. This value must be less than `container_memory` if set"
+  default     = 128
+}
+
+variable "port_mappings" {
+  type = list(object({
+    containerPort = number
+    hostPort      = number
+    protocol      = string
+  }))
+  description = "The port mappings to configure for the container. This is a list of maps. Each map should contain \"containerPort\", \"hostPort\", and \"protocol\", where \"protocol\" is one of \"tcp\" or \"udp\". If using containers in a task with the awsvpc or host network mode, the hostPort can either be left blank or set to the same value as the containerPort"
+  default     = []
+}
+
+variable "container_environment" {
+  type = list(object({
+    name  = string
+    value = string
+  }))
+  description = "The environment variables to pass to the container. This is a list of maps"
+  default     = []
+}
+
+variable "map_container_environment" {
+  type        = map(string)
+  description = "The environment variables to pass to the container. This is a map of string: {key: value}. `environment` overrides `map_environment`"
+  default     = null
+}
+
+variable "secrets" {
+  type = list(object({
+    name      = string
+    valueFrom = string
+  }))
+  description = "The secrets to pass to the container. This is a list of maps"
+  default     = []
+}
+
+variable "map_secrets" {
+  type        = map(string)
+  description = "The secrets variables to pass to the container. This is a map of string: {key: value}. map_secrets overrides secrets"
+  default     = null
+}
+
+# https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_HealthCheck.html
+variable "healthcheck" {
+  type = object({
+    command     = list(string)
+    retries     = number
+    timeout     = number
+    interval    = number
+    startPeriod = number
+  })
+  description = "A map containing command (string), timeout, interval (duration in seconds), retries (1-10, number of times to retry before marking container unhealthy), and startPeriod (0-300, optional grace period to wait, in seconds, before failed healthchecks count toward retries)"
+  default     = null
+}
+
+variable "application_label_order" {
+  type        = list(string)
+  default     = null
+  description = <<-EOT
+    The order in which the labels (ID elements) appear in the `id`.
+    Defaults to ["namespace", "environment", "stage", "name", "attributes"].
+    You can omit any of the 6 labels ("tenant" is the 6th), but at least one must be present.
+    EOT
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region"
+  default     = null
+}
+
+variable "log_retention_in_days" {
+  type        = number
+  description = "The number of days to retain logs for the log group"
+  default     = 1
+}
+
+variable "log_driver" {
+  type        = string
+  description = "The log driver to use for the container. If using Fargate launch type, only supported value is awslogs"
+  default     = "awsfirelens"
+}
+
+variable "ecs_cluster_arn" {
+  type        = string
+  description = "The ECS Cluster ARN where ECS Service will be provisioned"
+}
+
+variable "cloudwatch_log_group_enabled" {
+  type        = bool
+  description = "A boolean to disable cloudwatch log group creation"
+  default     = true
+}
+
+variable "task_count" {
+  type        = number
+  description = "The number of tasks to create based on the TaskDefinition."
+  default     = null
+}
+
+variable "schedule_expression" {
+  type        = string
+  default     = ""
+  description = "The scheduling expression. For example, cron(0 20 * * ? *) or rate(5 minutes). At least one of schedule_expression or event_pattern is required. Can only be used on the default event bus."
+}
+
+variable "cloudwatch_event_policy_arns" {
+  type        = string
+  default     = ""
+  description = "The Amazon Resource Name (ARN) of the IAM role to be used for this target when the rule is triggered."
+}
+
+variable "is_enabled" {
+  type        = bool
+  description = "Whether the rule should be enabled."
+  default     = true
+}
